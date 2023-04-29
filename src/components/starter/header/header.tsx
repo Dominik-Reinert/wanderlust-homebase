68b160b1279112a222b883ed5787011e3ecd23bd
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-04-29 16:55:56 +0200

    main: properly configure tailwind

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -5,6 +5,7 @@ import { Text } from "~/components/text";
 export default component$(() => {
   return (
     <header class={styles.header}>
+      <div class="hidden p-10 font-saver text-sm text-base text-lg text-xl text-2xl"></div>
       <Text size="large">Merzig mingle matrix</Text>
     </header>
   );
