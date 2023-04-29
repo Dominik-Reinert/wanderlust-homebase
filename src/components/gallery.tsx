070365d6243d9d3dff6638f5aa17d2f2e404ded9
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-04-29 17:32:39 +0200

    main: add gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,16 +1,37 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { Text } from "./text";
+import navArrowLeft from "iconoir/icons/nav-arrow-left.svg";
+import navArrowRight from "iconoir/icons/nav-arrow-right.svg";
 
 interface GalleryProps {
   items: ItemProps[];
 }
 
 export const Gallery = component$((props: GalleryProps) => {
+  const currentImage = useSignal(0);
   return (
-    <div>
-      {props.items.map((item) => (
-        <Item key={item.title} {...item} />
-      ))}
+    <div class="relative">
+      <img
+        class="absolute top-1/2 left-0"
+        src={navArrowLeft}
+        alt="nav arrow left"
+        onClick$={() => {
+          if (currentImage.value >= props.items.length - 1) {
+            currentImage.value--;
+          }
+        }}
+      />
+      <img
+        class="absolute top-1/2 right-0"
+        src={navArrowRight}
+        alt="nav arrow right"
+        onClick$={() => {
+          if (currentImage.value < props.items.length) {
+            currentImage.value++;
+          }
+        }}
+      />
+      <Item {...props.items[currentImage.value]} />
     </div>
   );
 });
