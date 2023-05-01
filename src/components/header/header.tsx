8919e935a4bd82c406f98350ed14231ffce3dbfb
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-01 10:40:42 +0200

    main: add back button to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,25 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, QRL } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 import { Text } from "~/components/text";
+import { ArrowLeftIcon } from "~/integrations/react/iconoir";
 
 export const Header = component$(() => {
+  const loc = useLocation();
+  const needsBack = loc.url.pathname !== "/";
   return (
-    <header class={`min-h-10vh p-3`}>
+    <header class={`min-h-10vh p-3 relative bg-sky-300 `}>
       <div class="hidden p-10 font-saver text-sm text-base text-lg text-xl text-2xl text-sky-400 text-sky-600 bg-white border-sky-400 border-sky-600 rounded border-2 text-white bg-sky-600"></div>
-      <Text size="large">Merzig mingle matrix</Text>
+      {needsBack ? (
+        <a
+          class="absolute flex items-center top-1/4 h-1/2 cursor-pointer"
+          href="/"
+        >
+          <ArrowLeftIcon />
+        </a>
+      ) : null}
+      <div class="p-3 pl-10">
+        <Text size="large">Merzig mingle matrix</Text>
+      </div>
     </header>
   );
 });
--- src/integrations/react/iconoir.tsx
+++ src/integrations/react/iconoir.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource react */
 
 import { qwikify$ } from "@builder.io/qwik-react";
-import { Archery, BookStack, ChatBubbleEmpty, CutSolidWithCurve, HouseRooms, Wifi } from "iconoir-react";
+import { Archery, ArrowLeft, BookStack, ChatBubbleEmpty, CutSolidWithCurve, HouseRooms, Wifi } from "iconoir-react";
 
 export const WifiIcon = qwikify$(Wifi);
 export const CutSolidWithCurveIcon = qwikify$(CutSolidWithCurve);
@@ -9,3 +9,4 @@ export const ArcheryIcon = qwikify$(Archery);
 export const BookStackIcon = qwikify$(BookStack);
 export const HouseRoomsIcon = qwikify$(HouseRooms);
 export const ChatBubbleEmptyIcon = qwikify$(ChatBubbleEmpty);
+export const ArrowLeftIcon = qwikify$(ArrowLeft);
