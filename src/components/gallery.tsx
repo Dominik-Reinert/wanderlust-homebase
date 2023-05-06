2b269cf031a0d8186452f2b4f054d3b7309f5692
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-06 11:35:01 +0200

    main: change text to white in the gallerey

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -58,7 +58,7 @@ const Item = component$(
     return (
       <div
         key={title}
-        class={`w-full min-h-30vh bg-cover bg-center flex flex-col p-5 md:p-7 md:min-h-30vh bg-cover bg-no-repeat`}
+        class={`w-full min-h-30vh bg-cover bg-center text-white flex flex-col p-5 md:p-7 md:min-h-30vh bg-cover bg-no-repeat`}
         style={{
           background: `url(${imageUrl})`,
         }}
