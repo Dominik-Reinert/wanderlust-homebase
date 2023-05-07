c6702e261e19510571797dcec534018460e83d98
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2023-05-07 15:14:30 +0200

    main: split hte modal do work with two stores

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,9 +1,30 @@
-import { component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
+import {
+  JSXChildren,
+  component$,
+  useStore,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import { Text } from "./text";
 
-export const modalStore = { open: false };
+export const modalStore: {
+  open: boolean;
+} = { open: false };
+
+export const modalContentStore: {
+  title: string;
+  content: JSXChildren;
+} = {
+  title: "",
+  content: (
+    <>
+      <div>hello</div>
+    </>
+  ),
+};
 
 export const Modal = component$(() => {
   const store = useStore(modalStore);
+  const contentStore = useStore(modalContentStore);
   useVisibleTask$(() => {
     if (store.open) {
       document.body.style.overflow = "hidden";
@@ -17,7 +38,12 @@ export const Modal = component$(() => {
       class="absolute z-50 inset-0 bg-black bg-opacity-25 w-full h-full"
       style={{ top: `${window.scrollY}px` }}
     >
-      hello modal
+      <div class="flex flex-col gap-5 m-5 p-5 bg-white">
+        <Text size="huge" bold mono>
+          {contentStore.title}
+        </Text>
+        <div>{contentStore.content}</div>
+      </div>
     </div>
   ) : null;
 });
